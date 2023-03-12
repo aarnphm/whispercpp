03306d7ea0d7e11a5983b89ba7510de0e8c4984e
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-03-12 09:18:21 -0700

    format
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- src/whispercpp/api_cpp2py_export.cc
+++ src/whispercpp/api_cpp2py_export.cc
@@ -1,12 +1,12 @@
 #include "api_cpp2py_export.h"
 
 WavFileWrapper WavFileWrapper::load_wav_file(const char *filename) {
-  std::vector<float> pcmf32;
-  std::vector<std::vector<float>> pcmf32s;
-  if (!::read_wav(filename, pcmf32, pcmf32s, false)) {
-    throw std::runtime_error("Failed to load wav file");
-  }
-  return WavFileWrapper(&pcmf32, &pcmf32s);
+    std::vector<float> pcmf32;
+    std::vector<std::vector<float>> pcmf32s;
+    if (!::read_wav(filename, pcmf32, pcmf32s, false)) {
+        throw std::runtime_error("Failed to load wav file");
+    }
+    return WavFileWrapper(&pcmf32, &pcmf32s);
 }
 
 namespace py = pybind11;
@@ -15,66 +15,66 @@ using namespace pybind11::literals;
 namespace whisper {
 
 PYBIND11_MODULE(api_cpp2py_export, m) {
-  m.doc() = "Python interface for whisper.cpp";
+    m.doc() = "Python interface for whisper.cpp";
 
-  // NOTE: default attributes
-  m.attr("SAMPLE_RATE") = py::int_(WHISPER_SAMPLE_RATE);
-  m.attr("N_FFT") = py::int_(WHISPER_N_FFT);
-  m.attr("N_MEL") = py::int_(WHISPER_N_MEL);
-  m.attr("HOP_LENGTH") = py::int_(WHISPER_HOP_LENGTH);
-  m.attr("CHUNK_SIZE") = py::int_(WHISPER_CHUNK_SIZE);
+    // NOTE: default attributes
+    m.attr("SAMPLE_RATE") = py::int_(WHISPER_SAMPLE_RATE);
+    m.attr("N_FFT") = py::int_(WHISPER_N_FFT);
+    m.attr("N_MEL") = py::int_(WHISPER_N_MEL);
+    m.attr("HOP_LENGTH") = py::int_(WHISPER_HOP_LENGTH);
+    m.attr("CHUNK_SIZE") = py::int_(WHISPER_CHUNK_SIZE);
 
-  // whisper_token_data -> TokenData
-  py::class_<whisper_token_data>(m, "TokenData", "Data for the token")
-      .def_readonly("id", &whisper_token_data::id)
-      .def_readonly("tid", &whisper_token_data::tid)
-      .def_readonly("p", &whisper_token_data::p)
-      .def_readonly("plog", &whisper_token_data::plog)
-      .def_readonly("pt", &whisper_token_data::pt)
-      .def_readonly("ptsum", &whisper_token_data::ptsum)
-      .def_readonly("t0", &whisper_token_data::t0)
-      .def_readonly("t1", &whisper_token_data::t1)
-      .def_readonly("vlen", &whisper_token_data::vlen)
-      .def("__repr__", [](const whisper_token_data &t) {
-        std::stringstream s;
-        s << "("
-          << "id=" << t.id << ", "
-          << "tid=" << t.tid << ", "
-          << "p=" << t.p << ", "
-          << "plog=" << t.plog << ", "
-          << "pt=" << t.pt << ", "
-          << "ptsum=" << t.ptsum << ", "
-          << "t0=" << t.t0 << ", "
-          << "t1=" << t.t1 << ", "
-          << "vlen=" << t.vlen << ")";
+    // whisper_token_data -> TokenData
+    py::class_<whisper_token_data>(m, "TokenData", "Data for the token")
+        .def_readonly("id", &whisper_token_data::id)
+        .def_readonly("tid", &whisper_token_data::tid)
+        .def_readonly("p", &whisper_token_data::p)
+        .def_readonly("plog", &whisper_token_data::plog)
+        .def_readonly("pt", &whisper_token_data::pt)
+        .def_readonly("ptsum", &whisper_token_data::ptsum)
+        .def_readonly("t0", &whisper_token_data::t0)
+        .def_readonly("t1", &whisper_token_data::t1)
+        .def_readonly("vlen", &whisper_token_data::vlen)
+        .def("__repr__", [](const whisper_token_data &t) {
+            std::stringstream s;
+            s << "("
+              << "id=" << t.id << ", "
+              << "tid=" << t.tid << ", "
+              << "p=" << t.p << ", "
+              << "plog=" << t.plog << ", "
+              << "pt=" << t.pt << ", "
+              << "ptsum=" << t.ptsum << ", "
+              << "t0=" << t.t0 << ", "
+              << "t1=" << t.t1 << ", "
+              << "vlen=" << t.vlen << ")";
 
-        return s.str();
-      });
+            return s.str();
+        });
 
-  py::enum_<whisper_sampling_strategy>(m, "StrategyType")
-      .value("SAMPLING_GREEDY",
-             whisper_sampling_strategy::WHISPER_SAMPLING_GREEDY)
-      .value("SAMPLING_BEAM_SEARCH",
-             whisper_sampling_strategy::WHISPER_SAMPLING_BEAM_SEARCH)
-      .export_values();
+    py::enum_<whisper_sampling_strategy>(m, "StrategyType")
+        .value("SAMPLING_GREEDY",
+               whisper_sampling_strategy::WHISPER_SAMPLING_GREEDY)
+        .value("SAMPLING_BEAM_SEARCH",
+               whisper_sampling_strategy::WHISPER_SAMPLING_BEAM_SEARCH)
+        .export_values();
 
-  m.def("load_wav_file", &WavFileWrapper::load_wav_file, "filename"_a,
-        py::return_value_policy::reference);
-
-  py::class_<WavFileWrapper>(m, "Wavfile",
-                             "A light wrapper for the processed wav file.")
-      .def_property_readonly(
-          "stereo", [](WavFileWrapper &self) { return self.stereo; },
-          py::return_value_policy::reference)
-      .def_property_readonly(
-          "mono", [](WavFileWrapper &self) { return self.mono; },
+    m.def("load_wav_file", &WavFileWrapper::load_wav_file, "filename"_a,
           py::return_value_policy::reference);
 
-  // NOTE: export Context API
-  ExportContextApi(m);
+    py::class_<WavFileWrapper>(m, "Wavfile",
+                               "A light wrapper for the processed wav file.")
+        .def_property_readonly(
+            "stereo", [](WavFileWrapper &self) { return self.stereo; },
+            py::return_value_policy::reference)
+        .def_property_readonly(
+            "mono", [](WavFileWrapper &self) { return self.mono; },
+            py::return_value_policy::reference);
 
-  // NOTE: export Params API
-  ExportSamplingStrategiesApi(m);
-  ExportParamsApi(m);
+    // NOTE: export Context API
+    ExportContextApi(m);
+
+    // NOTE: export Params API
+    ExportSamplingStrategiesApi(m);
+    ExportParamsApi(m);
 }
 }; // namespace whisper
--- src/whispercpp/api_cpp2py_export.h
+++ src/whispercpp/api_cpp2py_export.h
@@ -22,26 +22,26 @@ namespace whisper {
 // std::make_unique for C++11
 // https://stackoverflow.com/a/17902439/8643197
 template <class T> struct _Unique_if {
-  typedef std::unique_ptr<T> _Single_object;
+    typedef std::unique_ptr<T> _Single_object;
 };
 
 template <class T> struct _Unique_if<T[]> {
-  typedef std::unique_ptr<T[]> _Unknown_bound;
+    typedef std::unique_ptr<T[]> _Unknown_bound;
 };
 
 template <class T, size_t N> struct _Unique_if<T[N]> {
-  typedef void _Known_bound;
+    typedef void _Known_bound;
 };
 
 template <class T, class... Args>
 typename _Unique_if<T>::_Single_object make_unique(Args &&...args) {
-  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 template <class T>
 typename _Unique_if<T>::_Unknown_bound make_unique(size_t n) {
-  typedef typename std::remove_extent<T>::type U;
-  return std::unique_ptr<T>(new U[n]());
+    typedef typename std::remove_extent<T>::type U;
+    return std::unique_ptr<T>(new U[n]());
 }
 
 template <class T, class... Args>
@@ -51,25 +51,25 @@ typename _Unique_if<T>::_Known_bound make_unique(Args &&...) = delete;
 // See https://github.com/pybind/pybind11/issues/1042#issuecomment-642215028
 template <typename Sequence>
 inline py::array_t<typename Sequence::value_type> as_pyarray(Sequence &&seq) {
-  auto size = seq.size();
-  auto data = seq.data();
-  std::unique_ptr<Sequence> seq_ptr =
-      whisper::make_unique<Sequence>(std::move(seq));
-  auto capsule = py::capsule(seq_ptr.get(), [](void *p) {
-    std::unique_ptr<Sequence>(reinterpret_cast<Sequence *>(p));
-  });
-  seq_ptr.release();
-  return py::array(size, data, capsule);
+    auto size = seq.size();
+    auto data = seq.data();
+    std::unique_ptr<Sequence> seq_ptr =
+        whisper::make_unique<Sequence>(std::move(seq));
+    auto capsule = py::capsule(seq_ptr.get(), [](void *p) {
+        std::unique_ptr<Sequence>(reinterpret_cast<Sequence *>(p));
+    });
+    seq_ptr.release();
+    return py::array(size, data, capsule);
 }
 } // namespace whisper
 
 struct WavFileWrapper {
-  py::array_t<float> mono;
-  std::vector<std::vector<float>> stereo;
+    py::array_t<float> mono;
+    std::vector<std::vector<float>> stereo;
 
-  WavFileWrapper(std::vector<float> *mono,
-                 std::vector<std::vector<float>> *stereo)
-      : mono(whisper::as_pyarray(std::move(*mono))), stereo(*stereo){};
+    WavFileWrapper(std::vector<float> *mono,
+                   std::vector<std::vector<float>> *stereo)
+        : mono(whisper::as_pyarray(std::move(*mono))), stereo(*stereo){};
 
-  static WavFileWrapper load_wav_file(const char *filename);
+    static WavFileWrapper load_wav_file(const char *filename);
 };
